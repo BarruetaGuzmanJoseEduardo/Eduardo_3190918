eb92b1aa88a863fd894c4a46a58b07992e75cf83
BarruetaGuzmanJoseEduardo <eduardobarrueta94@aragon.unam.mx>
2024-08-26 23:00:01 -0600

    logrado

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('El script está funcionando');
+let nombre = prompt('Mi nombre es Eduardo, ¿cómo te llamas?');
+let edad = parseInt(prompt('Tengo 20 años, ¿cuántos años tienes?'));
+
+console.log("El nombre es", nombre);
+console.log("La edad es", edad);
+

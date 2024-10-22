c7108cfc52f3539a09c2101c60ed05a3f80739ad
BarruetaGuzmanJoseEduardo <111659504+BarruetaGuzmanJoseEduardo@users.noreply.github.com>
2024-10-21 21:47:08 -0600

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-alert('El script está funcionando');
-let nombre = prompt('Mi nombre es Eduardo, ¿cómo te llamas?');
-let edad = parseInt(prompt('Tengo 20 años, ¿cuántos años tienes?'));
-
-console.log("El nombre es", nombre);
-console.log("La edad es", edad);
-
